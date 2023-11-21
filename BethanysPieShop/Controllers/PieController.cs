b0f0729f87eff9472483725ceb702772eb0789dc
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-11-21 14:59:13 +0100

    Using a View Model

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,23 +1,27 @@
 ﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
     {
-        private readonly IPieRepository _pieRespotory;
+        private readonly IPieRepository _pieRepository;
         private readonly ICategoryRepository _categoryRepository;
 
         public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
-            _pieRespotory = pieRepository;
+            _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         }
 
         public IActionResult List()
         {
-            ViewBag.CurrentCategory = "Cheese cakes";
-            return View(_pieRespotory.AllPies);
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            //return View(_pieRespotory.AllPies);
+
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cales");
+            return View(pieListViewModel);
         }
     }
 }
