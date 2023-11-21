c420665e78be0b518f7d48ba1dab003f9dbb8b92
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-11-21 18:19:47 +0100

    Adding EF core to the Application

---
+++ BethanysPieShop/Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+    public class BethanysPieShopDbContext : DbContext
+    {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext>
+            optoins) : base(optoins)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+
+            
+     }
+
+}
