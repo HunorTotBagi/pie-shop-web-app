cdb0f13d4cd0ae4a3263421dc5eb262ebbe00703
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-11-21 22:23:47 +0100

    Adding Routes to the Application

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -22,5 +22,13 @@ namespace BethanysPieShop.Controllers
             PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
             return View(pieListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
