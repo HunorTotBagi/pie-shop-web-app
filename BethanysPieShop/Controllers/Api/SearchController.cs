6fce2bf8d2accd4bea1960573b9c4cbaa8c570f4
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-11-22 22:44:39 +0100

    Setting up the API

---
+++ BethanysPieShop/Controllers/Api/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using BethanysPieShop.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IPieRepository _pieRepositroy;
+
+        public SearchController(IPieRepository pieRepositroy)
+        {
+            _pieRepositroy = pieRepositroy;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+
+        }
+    }
+}
